c024ef1e774904e399623192c5dd610f1de16e64
Григорьев Илья <giv2005@bk.ru>
2020-12-16 00:03:51 +0300

    add template startapp and db tables

--- VotingService/settings.py
+++ VotingService/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'VotingService.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': ['main/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- VotingService/urls.py
+++ VotingService/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from main.views import main_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_page),
 ]
