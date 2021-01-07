2f9c99f594489bb248e547bd94f7ff9c9d4274b5
Il'ya Grigorev <ilya_grig@MacBook-Pro-Ilya.local>
2021-01-07 22:39:24 +0300

    upgrade votedusers serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,15 +1,5 @@
 from typing import Dict, Any
 
-from rest_framework.serializers import ModelSerializer
-
-from api.models import Voting, Options, VotedUsers
-
-
-class VotingSerializer(ModelSerializer):
-    class Meta:
-        model = Voting
-        fields = ['title', 'description', 'user', 'start_date', 'end_date', 'status', 'type', 'id']
-
 
 def serialize_vote(vote) -> Dict[str, Any]:
     return {
@@ -24,12 +14,6 @@ def serialize_vote(vote) -> Dict[str, Any]:
     }
 
 
-class OptionsSerializer(ModelSerializer):
-    class Meta:
-        model = Options
-        fields = ['text', 'voting', 'id']
-
-
 def serialize_option(option) -> Dict[str, Any]:
     return {
         'text': option.text,
@@ -37,16 +21,14 @@ def serialize_option(option) -> Dict[str, Any]:
     }
 
 
-class VotedUsersSerializer(ModelSerializer):
-    class Meta:
-        model = VotedUsers
-        fields = ['user', 'option', 'id']
-
-
 def serialize_voteduser(voteduser) -> Dict[str, Any]:
     return {
-        'name': voteduser.user.username,
-        'id': voteduser.user.id
+        'user': {
+            'name': voteduser.user.username,
+            'id': voteduser.user.id
+        },
+        'option id': voteduser.option_id,
+        'voting_id': voteduser.option.voting_id
     }
 
 
