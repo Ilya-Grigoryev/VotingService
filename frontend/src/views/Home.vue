18d56bb04212a70cfa4c9a5b493d6dc1e28f932f
alex <aleshabulanov@mail.ru>
2021-01-14 16:47:04 +0300

    add celendar in home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,16 +1,42 @@
 <template>
   <div class="home">
+    <div  class="right">
+    <v-row>
+<!--    <v-col cols="6">-->
+<!--    <v-card-->
+<!--            v-for="(voting, index) in voting_list" :key="index"-->
+<!--            class="mx-auto pa-3 ma-3"-->
+<!--            elevation="4"-->
+<!--            outlined-->
+<!--            width="65%">-->
+<!--      <v-btn @click="$router.push(`/poll/${voting.id}/`)" icon depressed color="teal" style="position: absolute; right: 10px;">-->
+<!--        <v-icon>mdi-arrow-expand-all</v-icon>-->
+<!--      </v-btn>-->
+<!--      <Poll v-bind="voting" :user="user"/>-->
+<!--    </v-card>-->
+<!--    </v-col>-->
+    <v-col>
     <v-card
-            v-for="(voting, index) in voting_list" :key="index"
-            class="mx-auto pa-3 ma-3"
             elevation="4"
             outlined
-            width="65%">
-      <v-btn @click="$router.push(`/poll/${voting.id}/`)" icon depressed color="teal" style="position: absolute; right: 10px;">
-        <v-icon>mdi-arrow-expand-all</v-icon>
-      </v-btn>
-      <Poll v-bind="voting" :user="user"/>
-    </v-card>
+            width="17%">
+    <v-time-picker class="pa-1"
+                   position="left"
+                   v-model="time"
+                   format="24hr"
+                   use-seconds
+                   header-color="teal"
+    ></v-time-picker>
+    <v-date-picker class="pa-1"
+                    v-model="picker"
+                   position="left"
+                   color="teal"
+                   header-color="teal"
+    ></v-date-picker>
+      </v-card>
+    </v-col>
+    </v-row>
+  </div>
   </div>
 </template>
 
@@ -18,16 +44,17 @@
 
 <script>
   // @ is an alias to /src
-  import Poll from '../components/Poll.vue'
+  // import Poll from '../components/Poll.vue'
 
   export default {
     name: 'Home',
     props: ['user'],
-    components: {
-      Poll
-    },
+    // components: {
+    //   Poll
+    // },
     data: () => ({
-      voting_list: []
+      voting_list: [],
+      picker: new Date().toISOString().substr(0, 10),
     }),
     methods: {
       get_voting_list() {
